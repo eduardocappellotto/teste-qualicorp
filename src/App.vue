eb4970204b3b201924a4ccad1b46edc9ba44ce8a
eduardocappellotto <eduardocappellotto@gmail.com>
2021-03-29 05:27:38 -0300

    Configuração utilizando os componentes criados e as libs adicionadas.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header/>
+    <router-view />
   </div>
 </template>
+<script>
+import Header from './components/Header.vue';
+import './assets/scss/base.scss';
 
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body, html {
+background: rgb(0,0,0);
+background: radial-gradient(circle, rgba(0,0,0,1) 0%,
+ rgba(15,31,42,1) 25%,
+ rgba(28,59,81,1) 50%,
+ rgba(43,90,123,1) 75%,
+ rgba(57,120,165,1) 100%);
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
+import Toasted from 'vue-toasted';
 import App from './App.vue';
 import router from './router';
 
+require('dotenv').config();
+
 Vue.config.productionTip = false;
+Vue.use(Toasted, {
+  iconPack: 'fontawesome',
+});
 
 new Vue({
   router,
