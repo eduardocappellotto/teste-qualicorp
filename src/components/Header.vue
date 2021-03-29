aa08a241e26db70f4405f8cf35cdb61175491010
eduardocappellotto <eduardocappellotto@gmail.com>
2021-03-29 05:28:15 -0300

    criação de componente simples de header

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="u-flex grid-12 header">
+   <img alt="Logo da Qualicorp" src="../assets/qualicorp-logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang="scss" scoped>
+ .header {
+   background-color: whitesmoke;
+ }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="u-flex justify-center">
+  <div class="search-container">
+    <div class="u-flex u-flexWrap justify-center">
+     <img :src="medicineLogo" alt="Desenho de um médico e uma médica">
+    <SearchFilters @search="onSearch"/>
+    </div>
+    <SearchResults v-if="planosList && planosList.total" :planosList="planosList"/>
   </div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import SearchFilters from '@/components/SearchFilters.vue';
+import SearchResults from '@/components/SearchResults.vue';
+import medicine from '../assets/medicine.svg';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    SearchFilters,
+    SearchResults,
+  },
+
+  data() {
+    return {
+      planosList: null,
+    };
+  },
+
+  computed: {
+    medicineLogo() {
+      return medicine;
+    },
+  },
+
+  methods: {
+    onSearch(value) {
+      this.planosList = value;
+    },
   },
 };
 </script>
+<style lang="scss" scoped>
+.search-container{
+  margin: 5rem 2rem;
+}
+
+img{
+  width: 400px;
+  background-color: whitesmoke;
+  padding: 1.5rem;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+}
+
+@media only screen and (max-width: 600px) {
+  img {
+    display: none;
+  }
+}
+</style>
