1d52d6b0684cbb8e2ae1dff3d710365912216f20
eduardocappellotto <eduardocappellotto@gmail.com>
2021-03-29 05:24:40 -0300

    adicionado arquivo de services - utilizado axios

---
+++ src/services/services.js
@@ -0,0 +1,80 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://lb-aws-1105894158.sa-east-1.elb.amazonaws.com',
+});
+
+const getStates = async () => {
+  const response = await fetch('json/brazil.json');
+  const data = await response.json();
+  return data.estados.map((estado) => estado.sigla);
+};
+
+const getCitys = async (state) => {
+  const response = await fetch('json/brazil.json');
+  const data = await response.json();
+  const citys = data.estados.filter((estado) => state === estado.sigla);
+
+  return citys[0].cidades || [];
+};
+
+const getJobs = async (data) => {
+  const apiKey = await process.env.VUE_APP_API_KEY_LISTA_PROFISSAO;
+  let profissoes;
+  await api.get(`/profissao/${data.estado}/${data.cidade}?api-key=${apiKey}`,
+    {
+      crossorigin: true,
+    })
+    .then((response) => {
+      profissoes = response.data;
+    })
+    .catch((error) => {
+    // handle error
+      console.log(error);
+    });
+
+  return profissoes || [];
+};
+
+const getEntities = async (data) => {
+  const apiKey = await process.env.VUE_APP_API_KEY_LISTA_ENTIDADE;
+  let profissoes;
+  await api.get(`/entidade/${data.profissao}/${data.estado}/${data.cidade}?api-key=${apiKey}`,
+    {
+      crossorigin: true,
+    })
+    .then((response) => {
+      profissoes = response.data;
+    })
+    .catch((error) => { profissoes = error; });
+  return profissoes;
+};
+
+const getPlanos = async (data) => {
+  const apiKey = await process.env.VUE_APP_API_KEY_LISTA_PLANOS;
+  let profissoes;
+  await api.post(`/plano?api-key=${apiKey}`,
+    {
+      headers: { 'Content-Type': 'text/plain' },
+      crossorigin: true,
+      entidade: data.entidade,
+      uf: data.uf,
+      cidade: data.cidade,
+      datanascimento: data.datanascimento,
+    })
+    .then((response) => {
+      profissoes = response.data;
+    })
+    .catch((error) => error.message);
+  return profissoes;
+};
+
+export default {
+  getStates,
+  getCitys,
+  getJobs,
+  getEntities,
+  getPlanos,
+
+  api,
+};
