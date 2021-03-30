45628b3bcab5ca5e8c981d4f70beeceb20f48e6e
eduardocappellotto <eduardocappellotto@gmail.com>
2021-03-30 00:20:44 -0300

    adicionado rota de resultados da query

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Results from '../views/Results.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,12 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    name: 'Results',
+    path: '/results',
+    component: Results,
+    props: true,
   },
 ];
 
